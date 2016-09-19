7cbd0ba9662d1701e8124fc78e69aebeb3a1054f
MistaSierra <sierra.n.shaw@gmail.com>
2016-09-19 11:18:13 -0600

    hw 2.1

---
+++ HW-2-1/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas (700, 600);
+  background (0,0,0);
+}
+var x= 200
+
+function draw() {
+  ellipse (350,x,x,150);//head
+  ellipse (290,x,30,10);// left eye
+  ellipse (410,x,30,10);//right eye
+  line (270,220,430,220);//mouth
+}
