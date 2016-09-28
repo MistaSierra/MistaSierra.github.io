1d5df1180e740cf65a1cb382dd47ed48aa7d9719
MistaSierra <sierra.n.shaw@gmail.com>
2016-09-28 11:11:36 -0600

    h

--- HW-4-1/sketch.js
+++ HW-4-1/sketch.js
@@ -1,18 +1,18 @@
 var x = 0;
 var speed = 3;
-var circlesize = 100
+var circleSize = 100;
 
 
 function setup() {
   createCanvas (500,500);
   background(76, 59, 100);
-  frameRate (40)
+  frameRate (40);
 }
 
 function draw() {
   noStroke();
   fill (random(0,200), 0, random(0,150));
-  ellipse(x, height/2, circlesize, circlesize );
+  ellipse(x, height/2, circleSize, circleSize);
   
   textSize(25)
   text("hold down mouse to increase size of circle", 15,450); 
@@ -20,13 +20,14 @@ function draw() {
   if (x > width || x < 0) {
     speed = speed * -1;
   }
+  
   x = x + speed;
   
   if(mouseIsPressed) {
-    circlesize = circlesize + 1;
-    //else(){
-       //circlesize = circlesize - .5
-    }
+    circleSize = circleSize + 1;
+  } else if (circleSize > 10) {
+    circleSize = circleSize - .5;
+   }
     
   }
   
