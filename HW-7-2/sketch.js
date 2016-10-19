1ea5bc360f91ebf25b5ae5b7315c11502098d854
MistaSierra <Sierra.Shaw@umconnect.umt.edu>
2016-10-19 00:37:37 -0600

    l

--- HW-7-2/sketch.js
+++ HW-7-2/sketch.js
@@ -1,3 +1,30 @@
+function pacMan () {
+  //I am not so sure on how to format all these
+  //below are the variables
+  y= 300;
+  x= 400;
+  ySpeed= -3;
+  xSpeed= 4;
+
+//meant to create at most 5 pacman
+  for (//what is going on here; pacMan <5, pacMan++); {
+  noStroke();
+  fill(255,255,0);
+  ellipse(pacMan.x, pacMan.y, 24, 24);
+  }
+
+//bouncing pacMan
+  if (pacMan.x> width || pacMan.y< 0) {
+    pacMan.xSpeed = pacMan.ySpeed * -1;
+  }
+ 
+  if (pacMan.y > height || pacMan.y < 0) {
+    pacMan.ySpeed = pacMan.ySpeed * -.5;
+  }
+  
+  pacMan.x = pacMan.x + pacMan.xSpeed;
+  pacMan.y = pacMan.y + pacMan.ySpeed;
+}
 function setup() {
   createCanvas(600, 600);
   background (0);
@@ -5,5 +32,4 @@ function setup() {
 
 function draw() {
   pacMan ();
-
 }
