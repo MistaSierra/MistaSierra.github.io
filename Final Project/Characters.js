e0078ff36970390aa03b2e8c3455057109f7b02c
MistaSierra <Sierra.Shaw@umconnect.umt.edu>
2016-12-15 21:51:29 -0700

    k

--- Final Project/Characters.js
+++ Final Project/Characters.js
@@ -1,15 +1,15 @@
 //sketch won't load in browser
 //Player Character
 function Player () {
-    this.diameter = 20;
-    this.x = 20;
-    this.y = 20;
+  this.diameter = 15;
+  this.x = 20;
+  this.y = 20;
 }
 //player controls player by moving their mouse
 Player.prototype.draw = function (){
 
-    this.x = mouseX;
-    this.y = mouseY;
+  this.x = mouseX;
+  this.y = mouseY;
 
   fill (0,255,0);
   ellipse(this.x, this.y, this.diameter, this.diameter);
@@ -17,47 +17,47 @@ Player.prototype.draw = function (){
 };
 //function for creating an event with objects interact
 Player.prototype.collisionCheck = function ( objArr ) {
-    var distance = 0;
-    var minDist = 0;
-    for (var i = 0; i < objArr.length; i++) {
-        distance = dist(objArr[i].x, objArr[i].y, this.x, this.y);
-        minDist = this.diameter*0.5 + objArr[i].diameter*0.5;
+  var distance = 0;
+  var minDist = 0;
+  for (var i = 0; i < objArr.length; i++) {
+    distance = dist(objArr[i].x, objArr[i].y, this.x, this.y);
+    minDist = this.diameter*0.5 + objArr[i].diameter*0.5;
 
-        // check for a collision
-        if ( distance <= minDist ) {
+    // check for a collision
+    if ( distance <= minDist ) {
 
-            // check size relationship
-            //if the player is smaller than Enemy, they grow by 2
-            //if the player is larger than Enemy, they shrink by 5
-            if (objArr[i].diameter <= this.diameter) {
-                this.diameter = this.diameter - 5;
-            } else {
-                this.diameter = this.diameter + 2;
-            }
-            // respawn enemy
-            objArr[i].respawn();
-
-        }
+      // check size relationship
+      //if the player is smaller than Enemy, they grow by 2
+      //if the player is larger than Enemy, they shrink by 5
+      if (objArr[i].diameter <= this.diameter) {
+        this.diameter = this.diameter - 5;
+      } else {
+        this.diameter = this.diameter + 2;
+      }
+      // respawn enemy
+      objArr[i].respawn();
 
     }
+
+  }
 };
 
 //constructor method class type Enemies
 // Enemy Characters
-function Enemies (initXPos) {
+function Enemies (initXPos, idx) {
 
-//variables
+  //variables
   this.x = initXPos;
   this.y = 300;
-  this.diameter = random (50);
+  this.diameter = random (10,50);
   this.ySpeed= random(-10, 10);
   this.xSpeed= random(-10, 10);
+  this.idx = idx;
 
 }
 
 //draw function for Enemies class
-Enemies.prototype.draw = function () {
-  //something should go here......
+Enemies.prototype.draw = function (objArr) {
   this.move ();
   //this.timer();
 
@@ -66,30 +66,40 @@ Enemies.prototype.draw = function () {
   fill(255,0,0);
   ellipse(this.x, this.y, this.diameter, this.diameter);
 
-//Enemy object collision. if enemy's diameter is equal to another's they go in the opposite direction
+  //Enemy object collision. if enemy's diameter is equal to another's they go in the opposite direction
+  for (var i = 0; i < objArr.length; i++) {
+    var distance = 0;
+    var minDist = 0;
+    distance = dist(objArr[i].x, objArr[i].y, this.x, this.y);
+    minDist = this.diameter*0.5 + objArr[i].diameter*0.5;
+    // console.log(this.idx + " : " + distance + " : " + minDist);
 
-      if (  objArr[i].diameter == this.diameter ) {
-          this.yspeed = this.yspeed * -1;
-          this.xspeed = this.xspeed * -1;
-       } else {
-          this.diameter = this.diameter + 2;
-      }
+    // check for a collision
+    if ( distance <= minDist && this.idx != i ) {
+      this.ySpeed = this.ySpeed * -1;
+      this.xSpeed = this.xSpeed * -1;
 
+
+    }
+  }
+
+
+  // if (  objArr.diameter == this.diameter ) {
+  // } else {
+  //   this.diameter = this.diameter + 2;
+  // }
 };
 
-
 Enemies.prototype.move = function () {
-//boundaries for moving enemies. Should go in all directions
+  //boundaries for moving enemies. Should go in all directions
   if (this.x> width || this.x< 0) {
     this.xSpeed = this.xSpeed * -1;
-    this.ySpeed = this.ySpeed * -1;
+    // this.ySpeed = this.ySpeed * -1;
   }
 
   if (this.y > height || this.y < 0) {
-    this.xSpeed = this.xSpeed * -1;
+    // this.xSpeed = this.xSpeed * -1;
     this.ySpeed = this.ySpeed * -1;
-    }
-    this.ySpeed = this.ySpeed * -0.5;
   }
 
   this.x = this.x + this.xSpeed;
@@ -98,9 +108,9 @@ Enemies.prototype.move = function () {
 };
 //enemies "come back to life" when "defeated"
 Enemies.prototype.respawn = function () {
-    this.x = random(width);
-    this.y = random(height);
-    this.diameter = random(10, 50);
-    this.xSpeed = -1 * this.xSpeed;
-    this.ySpeed = -1 * this.ySpeed;
+  this.x = random(width);
+  this.y = random(height);
+  this.diameter = random(10, 50);
+  this.xSpeed = -1 * this.xSpeed;
+  this.ySpeed = -1 * this.ySpeed;
 };
--- Final Project/sketch.js
+++ Final Project/sketch.js
@@ -6,7 +6,7 @@ var musicRate = 0.7;
 var currTime = 60;
 //load music file
 function preload() {
-    music = loadSound('assets/Neoakushon.mp3');
+  music = loadSound('assets/Neoakushon.mp3');
 }
 
 function setup() {
@@ -15,8 +15,9 @@ function setup() {
   noCursor();
 
   for (var i = 0; i < numOfEnemy; i++) {
-    enemy.push( new Enemies( i * 100 + 100 ) );
+    enemy.push( new Enemies( i * 100 + 100, i ) );
   }
+  //create player
   player = new Player ();
 
   // music
@@ -26,20 +27,25 @@ function setup() {
 }
 
 function draw() {
+  createCanvas(500, 600);
   background (10,100,40);
 
+  fill(250)
+  textSize(15)
+  text ("SCORE:" + player.diameter,400,50)
+
   var currentEnemies;
+  //display player's diameter, that is their score
 
   player.draw();
   // check for collisions and adjust size
   player.collisionCheck( enemy );
 
-//create number of enemies
+  //create number of enemies
   for (var i = 0; i < enemy.length; i++) {
     currentEnemies = enemy[i];
-    currentEnemies.draw();
+    currentEnemies.draw(enemy);
   }
-
 
   timer();
 }
@@ -47,23 +53,38 @@ function draw() {
 //display timer or countdown
 function timer() {
 
-    push();
-    fill(255);
-    rect(width-110,10,100,20);
-    fill(0);
-    textSize(20);
-    text("time: "+currTime, width-100, 28);
-    pop();
+  push();
+  fill(255);
+  rect(width-110,10,100,20);
+  fill(0);
+  textSize(20);
+  text("time: "+currTime, width-100, 28);
+  pop();
 
-//increase speed of music
-    if (frameCount % 60 === 0) {
-        currTime--;
+  //increase speed of music
+  if (frameCount % 60 === 0) {
+    currTime--;
 
-        if (currTime < 20) {
-            musicRate = musicRate + musicRate * 0.05;
-            text(musicRate, 10, 10);
-            music.rate(musicRate);
-        }
+    if (currTime < 20) {
+      musicRate = musicRate + musicRate * 0.04;
+      text(musicRate, 10, 10);
+      music.rate(musicRate);
+      else {
+        musicRate = musicRate
+      }
     }
+  }
+
+   if(currTime <= -1){
+  //if(currTime <= 55){
+      reset();
+  }
+}
+
+
+//game restarts after the timer reaches "0"
+function reset() {
+  player.diameter = 15;
+  currTime= 60;
 
 }
